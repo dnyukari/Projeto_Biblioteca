8270c293126a861bfec240c4126217dfff7c9ce3
DANIELA YUKARI <134297464+dnyukari@users.noreply.github.com>
2025-05-12 16:09:55 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Define a URL base da API
 const api = axios.create({
-  baseURL: 'https://6720e05a98bbb4d93ca6769e.mockapi.io/movies', // URL da sua API
+  baseURL: window.REACT_APP_API_URL,
 });
 
 export default api;
